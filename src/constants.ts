9743268bbf0017ab0455adeda1126a64ee199849
Manish Prasad <manishvaishu25@gmail.com>
2026-03-25 17:08:06 +0530

    upgraded to typescript and esm support (#14)

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const ITEM_ALPHABET = '0123456789';
+export const OTP_LENGTH = 6;
+
+export const WEBHOOK_EVENTS = {
+  OTP_GENERATED: 'otp-generated',
+  OTP_BULK_GENERATED: 'bulk-otp-generated',
+} as const;
